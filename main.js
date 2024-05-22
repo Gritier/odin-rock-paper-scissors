5723bb7f38086ad8e292e170057bcbabdaf11f7f
Gritier <28902214+Gritier@users.noreply.github.com>
2024-05-22 12:04:56 +0200

    Add UI

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
 
@@ -11,43 +10,86 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    while(true) {
-        choice = prompt("What's your choice? ");
 
-        if (choice.toLowerCase() === 'rock'){
-            return 'Rock';
-        } else if ( choice.toLowerCase() === 'paper') {
-            return 'Paper';
-        } else if ( choice.toLowerCase() === 'scizzors'){
-            return 'Scizzors';
-        } else {
-            console.log('You inserted an invalid input! Retry!\n')
-        }
-    }
-}
-
-
-function playGame() {
+function playGame(){
     let humanScore = 0;
     let computerScore = 0;
 
+    function resetGame() {
+        const paragraph = document.querySelector("#winnerParagraph");
+        paragraph.remove()
+        updateScore(0,0);
+        humanScore = 0;
+        computerScore = 0;
+    }
+
     function playRound(humanChoice, computerChoice){
+        let lastTurn;
+        if (humanScore == 5 || computerScore == 5) {
+            resetGame();
+        }
         if (humanChoice === computerChoice) {
-            console.log("That's a draw!");
+            lastTurn = `${humanChoice} vs ${computerChoice} : It's a draw!`
         } else if ((humanChoice === 'Rock' && computerChoice === 'Scizzors') || (humanChoice === 'Paper' && computerChoice === 'Rock') || (humanChoice === 'Scizzors' && computerChoice === 'Paper')) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
             humanScore += 1;
+            lastTurn = `${humanChoice} vs ${computerChoice} : You get a point!`
         } else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
             computerScore += 1;
+            lastTurn = `${humanChoice} vs ${computerChoice} : Computer get a point!`
+        }
+
+        updateScore(humanScore, computerScore);
+
+        if (document.querySelector('#lastResultParagraph')) {
+            const turnResultParagraph = document.querySelector('#lastResultParagraph');
+            turnResultParagraph.textContent = lastTurn;
+        } else {
+            const turnResultParagraph = document.createElement('p');
+            turnResultParagraph.id = 'lastResultParagraph';
+            turnResultParagraph.textContent = lastTurn;
+            resultsDiv.appendChild(turnResultParagraph);
+        }
+
+        endGame();
+    }
+
+    function endGame() {
+        if (humanScore == 5 || computerScore == 5){
+            const winnerParagraph = document.createElement('p');
+
+            humanScore > computerScore ? winnerParagraph.textContent = "You are the WINNER!" : winnerParagraph.textContent = "The computer won the game!";
+            winnerParagraph.id = 'winnerParagraph';
+            resultsDiv.appendChild(winnerParagraph);
+            return true;
+        } else {
+            return false
         }
     }
 
-    for(let i = 0; i<5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log(`SCORE | Human: ${humanScore} | Computer: ${computerScore}`);
+    function updateScore(humanScore, computerScore){
+        resultsDiv.textContent = `SCORE | Human: ${humanScore} | Computer: ${computerScore}`;
     }
+
+    const body = document.querySelector('body');
+
+    const rockButton = document.createElement('button');
+    rockButton.textContent = 'Rock';
+    rockButton.addEventListener('click', () => {playRound('Rock', getComputerChoice())});
+    const paperButton = document.createElement('button');
+    paperButton.textContent = 'Paper';
+    paperButton.addEventListener('click', () => {playRound('Paper',getComputerChoice())});
+    const scizzorsButton = document.createElement('button');
+    scizzorsButton.textContent = 'Scizzors';
+    scizzorsButton.addEventListener('click', () => {playRound('Scizzors',getComputerChoice())});
+
+    body.appendChild(rockButton);
+    body.appendChild(paperButton);
+    body.appendChild(scizzorsButton);
+
+    const resultsDiv = document.createElement('div');
+    resultsDiv.textContent = `SCORE | Human: ${humanScore} | Computer: ${computerScore}`
+
+    body.appendChild(resultsDiv);
 }
 
-playGame()
+playGame();
